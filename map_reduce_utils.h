02e41c6040d7933cd94427169db03a9176443268
iris.safaka <iris.safaka@7627bd10-c805-11de-9e96-3193d784f102>
2009-11-28 19:10:32 +0000

    Added quicksort implementation in map_reduce_utils.c. Removed 'NOT_IMPLEMENTED' tags from map_reduce.c. Modified the GNUmakefile.

---
+++ map_reduce_utils.h
@@ -0,0 +1,18 @@
+/*
+ * map_reduce.h
+ *
+ *  Created on: Nov 28, 2009
+ *      Author: iris
+ */
+
+#ifndef MAP_REDUCE_H
+#define MAP_REDUCE_H
+
+#include "map_reduce.h"
+
+void sort_key_worker_mappings(MapKeyWorkerPairArray* array, map_key_compare_ptr compare);
+
+void sort_key_value_mappings(MapKeyValuePairArray* array, map_key_compare_ptr compare); 
+
+
+#endif
